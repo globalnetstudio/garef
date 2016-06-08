32b0c90170a7008ce06751d51e44a0c8de04ef76
Daniel González Briseño <d.gonzalez@globalnetstudio.com>
2016-06-08 10:43:46 -0500

    Se terminó la sección de nosotros

--- _views/about.php
+++ _views/about.php
@@ -1,25 +1,47 @@
 <div class="about">
-	<h2>Nosotros</h3>
+	<div class="heading">
+		<div class="wrapper">
+			<h2>Nosotros</h3>
+		</div> <!-- /.wrapper -->
+	</div> <!-- /.heading -->
 
-	<p>Circuitos Turísticos Garef es una empresa que nace en el 2001, como cualquier cliente que Rentaba Autobuses se dio cuenta de las necesidades y deficiencias de servicios. Fue por ello que surgió "Circuitos Turísticos" una empresa dedicada a la Transportacion Turistica de pasajeros con servicios en unidades exclusivamente de "Gran Lujo" siendo nuestro objetivo que nuestros "Clientes" queden Satisfechos con el servicio completamente, y lo principalmente primordial es hacer que su viaje sea un "Total placer de Viajar por Carretera".</p>
+	<div class="content">
+		<div class="description">
+			<p><strong>Circuitos Turísticos Garef</strong> es una empresa que nace en el 2001, como cualquier cliente que Rentaba Autobuses se dio cuenta de las necesidades y deficiencias de servicios. Fue por ello que surgió "Circuitos Turísticos" una empresa dedicada a la <span class="black">Transportación</span> Turistica de pasajeros con servicios en unidades exclusivamente de <span class="black">"Gran Lujo"</span> siendo nuestro objetivo que nuestros "Clientes" queden Satisfechos con el servicio completamente, y lo principalmente primordial es hacer que su viaje sea un <span class="black">"Total placer de Viajar por Carretera"</span>.</p>
+			
+			<p>Para ello buscamos proporcionar a nuestros Clientes las Unidades mas Modernas del Mercado, con la más <span class="black">"Alta calidad"</span> y lo más importante es <span class="black">"Satisfacer las necesidades de Nuestro Cliente"</span>.</p>
+			
+			<p>Nosotros somos Una Gran Opción en <span class="black">Transportación Turística</span> para nuestro cliente a Nivel nacional, proporcionamos <span class="black">logistica de Viaje</span>, así como operadores altamente capacitados, las unidades y operadores son monitoreados para llegar a su <span class="black">servicio siempre puntual</span>, así ustedes nunca se preocuparan por su servicio.</p>
+		</div> <!-- /.description -->
 
-	<p>Para ello buscamos proporcionar a nuestros Clientes las Unidades mas Modernas del Mercado, con la más "Alta calidad" y lo más importante es "Satisfacer las necesidades de Nuestro Cliente".</p>
-
-	<p>Nosotros somos Una Gran Opción en Transportación Turística para nuestro cliente a Nivel nacional, proporcionamos logistica de Viaje, así como operadores altamente capacitados, las unidades y operadores son monitoreados para llegar a su servicio siempre puntual, así ustedes nunca se preocuparan por su servicio.</p>
-
-	<h3>Nosotros les ofreceremos:</h3>
-
-	<ul>
-		<li>Compromiso</li>
-		<li>Puntualidad</li>
-		<li>Seguridad</li>
-		<li>Confiabilidad</li>
-		<li>Profesionalismo</li>
-		<li>Innovación</li>
-		<li>Actitud de servicio</li>
-	</ul>
-
-	<p>Por ello y mucho mas</p>
-	<p>"Circuitos Turísticos le da Más"</p>
-	<p>EXCELENTE VIAJE</p>
+		<div class="features clearfix">
+			<div class="features__left">
+				<h3>Nosotros les ofreceremos:</h3>
+				
+				<div class="row">
+					<div class="left-column">
+						<ul>
+							<li>Compromiso</li>
+							<li>Puntualidad</li>
+							<li>Seguridad</li>
+							<li>Confiabilidad</li>
+							<li>Profesionalismo</li>
+							<li>Innovación</li>
+							<li>Actitud de servicio</li>
+						</ul>
+					</div> <!-- /.column -->
+					<div class="right-column">
+						<p>Por ello y mucho mas</p>
+						<p>"Circuitos Turísticos le da Más"</p>
+						<p class="emphasis">¡EXCELENTE VIAJE!</p>
+					</div> <!-- /.column -->
+				</div> <!-- /.row -->
+			</div> <!-- /.features-left -->
+			<div class="features__right">
+				<div class="bus-image">
+					<img src="/images/nosotros/red-bus.jpg" alt="Camion Rojo">
+				</div> <!-- /.bus-image -->
+			</div> <!-- /.features-right -->
+		</div> <!-- /.features -->
+	</div> <!-- /.content -->
 </div> <!-- /.about -->
