dfaf6673dbeb6f3c2d43d9c4cf9c24793ba5bf1c
Daniel González Briseño <d.gonzalez@globalnetstudio.com>
2016-06-08 13:25:09 -0500

    Se terminó la sección de servicios

--- _views/services.php
+++ _views/services.php
@@ -1,7 +1,45 @@
 <div class="services">
-	<h2>Servicios</h2>
+	<div class="heading">
+		<div class="wrapper">
+			<h2>Servicios</h2>
+		</div> <!-- /.wrapper -->
+	</div> <!-- /.heading -->
 
-	<p>Transportamos a su equipo de trabajo a congresos, convenciones, reuniones, foros, conciertos y demás tipos de eventos con la mayor comodidad que ofrecen nuestras unidades de modelo reciente, las cuales cuentan con la calidad que sus pasajeros requieren.</p>
+	<div class="content">
+		<div class="description">
+			<p>Transportamos a su equipo de trabajo a <span class="black">congresos</span>, <span class="black">convenciones</span>, <span class="black">reuniones</span>, <span class="black">foros</span>, <span class="black">conciertos</span> y demás tipos de eventos con la mayor comodidad que ofrecen nuestras unidades de modelo reciente, las cuales cuentan con la calidad que sus pasajeros requieren.</p>
+			
+			<p>En el caso de los artistas, brindamos el traslado a eventos siempre garantizando la discreción absoluta, <span class="black">puntualidad</span>, <span class="black">seguridad y eficiencia</span>. Se pueden <span class="black">rentar camionetas</span> para concurrir a <span class="black">shows</span>, <span class="black">locaciones</span>, <span class="black">conciertos</span>, <span class="black">representaciones teatrales</span>.</p>
+		</div> <!-- /.description -->
 
-	<p>En el caso de los artistas, brindamos el traslado a eventos siempre garantizando la discreción absoluta, puntualidad, seguridad y eficiencia. Se pueden rentar camionetas para concurrir a shows, locaciones, conciertos, representaciones teatrales.</p>
+		<h3>Así mismo ofrecemos servicios de:</h3>
+
+		<div class="row">
+			<div class="column">
+				<img class="image-responsive service-img" src="/images/servicios/circuitos-turisticos.jpg" alt="Circuitos Turísticos">
+			</div>
+			<div class="column">
+				<img class="image-responsive service-img" src="/images/servicios/turismo-local.jpg" alt="Turísmo Local">
+			</div>
+			<div class="column">
+				<img class="image-responsive service-img" src="/images/servicios/eventos-especiales.jpg" alt="Eventos Especiales">
+			</div>
+		</div> <!-- /.row -->
+
+		<div class="row">
+			<div class="column">
+				<img class="image-responsive service-img" src="/images/servicios/eventos-deportivos.jpg" alt="Eventos Deportivos">
+			</div> <!-- /.column -->
+			<div class="column">
+				<img class="image-responsive service-img" src="/images/servicios/eventos-escolares.jpg" alt="Eventos Escolares">
+			</div> <!-- /.column -->
+			<div class="column">
+				<img class="image-responsive service-img" src="/images/servicios/transportacion-empresarial.jpg" alt="Transportación Empresarial">
+			</div> <!-- /.column -->
+		</div> <!-- /.row -->
+
+		<div class="excursions">
+			<img class="image-responsive excursion-img" src="/images/servicios/excusiones.jpg" alt="Excursiones">
+		</div> <!-- /.excursions -->
+	</div> <!-- /.content -->
 </div> <!-- /.services -->
