7df3feb598145488cb3e15cc51a771f495f1b998
Daniel González Briseño <d.gonzalez@globalnetstudio.com>
2016-06-08 12:30:12 -0500

    Se terminó la sección de unidades

--- _views/units.php
+++ _views/units.php
@@ -1,88 +1,132 @@
 <div class="units">
-	<h2>Unidades</h2>
+	<div class="heading">
+		<div class="wrapper">
+			<h2>Unidades</h2>
+		</div> <!-- /.wrapper -->
+	</div> <!-- /.heading -->
 
-	<p>Transportamos a su equipo de trabajo a congresos, convenciones, reuniones, foros, conciertos y demás tipos de eventos con la mayor comodidad que ofrecen nuestras unidades de modelo reciente, las cuales cuentan con la calidad que sus pasajeros requieren.</p>
+	<div class="content">
+		<div class="description">
+			<p>Transportamos a su equipo de trabajo a congresos, convenciones, reuniones, foros, conciertos y demás tipos de eventos con la mayor comodidad que ofrecen nuestras unidades de modelo reciente, las cuales cuentan con la calidad que sus pasajeros requieren.</p>
+			
+			<p>En el caso de los artistas, brindamos el traslado a eventos siempre garantizando la discreción absoluta, puntualidad, seguridad y eficiencia. Se pueden rentar camionetas para concurrir a shows, locaciones, conciertos, representaciones teatrales, entre otros.</p>
+		</div> <!-- /.description -->
 
-	<p>En el caso de los artistas, brindamos el traslado a eventos siempre garantizando la discreción absoluta, puntualidad, seguridad y eficiencia. Se pueden rentar camionetas para concurrir a shows, locaciones, conciertos, representaciones teatrales, entre otros.</p>
+		<h3>Irizar P.b</h3>
 
-	<h2>Irizar P.b</h2>
+		<p>Si quieres impresionar a tus pasajeros con una Unidad de Gran Lujo, una impactante presencia interior y exterior, un diseño único y aerodinámico totalmente inconfundible esta es una gran elección.</p>
 
-	<p>Si quieres impresionar a tus pasajeros con una Unidad de Gran Lujo, una impactante presencia interior y exterior, un diseño único y aerodinámico totalmente inconfundible esta es una gran elección.</p>
+		<div class="row">
+			<div class="left-column">
+				<h4 class="features-heading">Características Principales:</h4>
 
-	<h3>Características Principales:</h3>
+				<ul>
+					<li><i class="icon-circle"></i>1 y 2 Puertas de acceso.</li>
+					<li><i class="icon-circle"></i>49, 51, 53 y 55 Asientos de Tela o Tela con Piel.</li>
+					<li><i class="icon-circle"></i>Aire Acondicionado y Calefacción.</li>
+					<li><i class="icon-circle"></i>Auto de servicio de Café y Te opcional.</li> 
+					<li><i class="icon-circle"></i>Cinturones de seguridad</li>
+					<li><i class="icon-circle"></i>Pantallas y Dvd</li>
+					<li><i class="icon-circle"></i>Micrófono para guía</li>
+					<li><i class="icon-circle"></i>Música Ambiental</li>
+					<li><i class="icon-circle"></i>Con y sin Wifi</li>
+					<li><i class="icon-circle"></i>Totalmente Panorámico</li>
+					<li><i class="icon-circle"></i>Sanitario y Seguro de viajero.</li>
+				</ul>
+			</div> <!-- /.left-column -->
+			<div class="right-column">
+				<div class="white-bus">
+					<img class="image-responsive" src="/images/unidades/irizar.p.b.jpg" alt="Irizar P.B Camión Blanco">
+				</div>
+			</div> <!-- /.right-column -->
+		</div> <!-- /.row -->
 
-	<ul>
-		<li>1 y 2 Puertas de acceso.</li>
-		<li>49, 51, 53 y 55 Asientos de Tela o Tela con Piel.</li>
-		<li>Aire Acondicionado y Calefacción.</li>
-		<li>Auto de servicio de Café y Te opcional.</li> 
-		<li>Cinturones de seguridad</li>
-		<li>Pantallas y Dvd</li>
-		<li>Micrófono para guía</li>
-		<li>Música Ambiental</li>
-		<li>Con y sin Wifi</li>
-		<li>Totalmente Panorámico</li>
-		<li>Sanitario y Seguro de viajero.</li>
-	</ul>
+		<h3>Irizar I.6 Ejecutivo</h3>
 
-	<h3>Irizar I.6 Ejecutivo</h3>
+		<p>El Irizar I.6 Ejecutivo 2015 / 2016 es una unidad ejecutiva de exclusiva y excelente elegancia, destacada por su diseño y cuidado de cada detalle y confort. El diseño del interior del Irizar I.6 ofrece un ambiente agradable y sensación de amplitud pensado para el bienestar del conductor y sobre todo los “Pasajeros”.</p>
 
-	<p>El Irizar I.6 Ejecutivo 2015 / 2016 es una unidad ejecutiva de exclusiva y excelente elegancia, destacada por su diseño y cuidado de cada detalle y confort. El diseño del interior del Irizar I.6 ofrece un ambiente agradable y sensación de amplitud pensado para el bienestar del conductor y sobre todo los “Pasajeros”.</p>
+		<p>Su conducción es toda una experiencia y viajar en él se convierte en un auténtico placer.</p>
 
-	<p>Su conducción es toda una experiencia y viajar en él se convierte en un auténtico placer.</p>
+		<div class="row">
+			<div class="left-column">
+				<div class="red-bus">
+					<img class="image-responsive" src="/images/unidades/irizar-1.6.ejecutivo.jpg" alt="Irizar 1.6 Camión Rojo">
+				</div>
+			</div> <!-- /.left-column -->
+			<div class="right-column">
+				<h4 class="features-heading">Características Principales:</h4>
+				
+				<ul>
+					<li><i class="icon-circle"></i>Un Autobús de Gran Lujo y Comodidad</li>
+					<li><i class="icon-circle"></i>Wifi, Piso de madera</li>
+					<li><i class="icon-circle"></i>2 Puertas de acceso</li>
+					<li><i class="icon-circle"></i>Asientos de tela con Piel</li>
+					<li><i class="icon-circle"></i>Cargador para Celular, Tablet, Laptop etc.</li>
+					<li><i class="icon-circle"></i>Totalmente panorámico</li>
+					<li><i class="icon-circle"></i>Con 2 o 3 Ejes</li>
+					<li><i class="icon-circle"></i>49 y 51 asientos de Tela con Piel</li>
+					<li><i class="icon-circle"></i>4 y 6 Pantallas, DVD y CD</li>
+					<li><i class="icon-circle"></i>Aire Acondicionado, Calefacción y W.C</li>
+					<li><i class="icon-circle"></i>Servicio de Café y Té (Costo Adicional)</li>
+					<li><i class="icon-circle"></i>Micrófono para Guía</li>
+					<li><i class="icon-circle"></i>Música Ambiental</li>
+					<li><i class="icon-circle"></i>Cinturones de seguridad </li>
+					<li><i class="icon-circle"></i>Seguro de Viajero</li>
+				</ul>
+			</div> <!-- /.right-column -->
+		</div> <!-- /.row -->
 
-	<h3>Características Principales:</h3>
+		<h3>Camioneta Sprinter</h3>
 
-	<ul>
-		<li>Un Autobús de Gran Lujo y Comodidad</li>
-		<li>Wifi, Piso de madera</li>
-		<li>2 Puertas de acceso</li>
-		<li>Asientos de tela con Piel</li>
-		<li>Cargador para Celular, Tablet, Laptop etc.</li>
-		<li>Totalmente panorámico</li>
-		<li>Con 2 o 3 Ejes</li>
-		<li>49 y 51 asientos de Tela con Piel</li>
-		<li>4 y 6 Pantallas, DVD y CD</li>
-		<li>Aire Acondicionado, Calefacción y W.C</li>
-		<li>Servicio de Café y Té (Costo Adicional)</li>
-		<li>Micrófono para Guía</li>
-		<li>Música Ambiental</li>
-		<li>Cinturones de seguridad </li>
-		<li>Seguro de Viajero</li>
-	</ul>
+		<p>Nuestras Camionetas de Gran Lujo con chofer son ideales para viajes de grupos empresariales, cuentan con puertas automáticas y aire acondicionado. Nuestro servicio siempre garantiza la discreción y el respeto que usted se merece.</p>
 
-	<h2>Camioneta Sprinter</h2>
+		<div class="row">
+			<div class="left-column">
+				<h4 class="features-heading">Características Principales:</h4>
 
-	<p>Nuestras Camionetas de Gran Lujo con chofer son ideales para viajes de grupos empresariales, cuentan con puertas automáticas y aire acondicionado. Nuestro servicio siempre garantiza la discreción y el respeto que usted se merece.</p>
+				<ul>
+					<li>Capacidad para 17 y 20 pasajeros</li>
+					<li>Aire acondicionado</li> 
+					<li>Calefacción</li>
+					<li>Equipo de audio, dvd, pantalla 32 pulgadas y micrófono.</li>
+					<li>Asientos reclinables</li>
+					<li>Cabeceras</li>
+					<li>Coderas</li>
+					<li>Cortinas</li>
+					<li>Seguro de Viajero</li>
+				</ul>
+			</div> <!-- /.left-column -->
+			<div class="right-column">
+				<div class="sprinter-trucks">
+					<img class="image-responsive" src="/images/unidades/camioneta-sprinter.jpg" alt="Camionetas Sprinter">
+				</div>
+			</div> <!-- /.right-column -->
+		</div>
 
-	<h3>Características Principales:</h3>
+		<h3>Suburban de Gran Lujo con Chofer</h3>
 
-	<ul>
-		<li>Capacidad para 17 y 20 pasajeros</li>
-		<li>Aire acondicionado</li> 
-		<li>Calefacción</li>
-		<li>Equipo de audio, dvd, pantalla 32 pulgadas y micrófono.</li>
-		<li>Asientos reclinables</li>
-		<li>Cabeceras</li>
-		<li>Coderas</li>
-		<li>Cortinas</li>
-		<li>Seguro de Viajero</li>
-	</ul>
+		<p>La Camioneta Suburban® combina grandeza con elegancia,<br>tiene la tecnología y seguridad que la convierten en un modelo a seguir.</p>
 
-	<h2>Suburban de Gran Lujo con Chofer</h2>
-
-	<p>La Camioneta Suburban® combina grandeza con elegancia, tiene la tecnología y seguridad que la convierten en un modelo a seguir.</p>
-
-	<h3>Características Principales:</h3>
-
-	<ul>
-		<li>Automática</li>
-		<li>Vidrios Eléctricos</li>
-		<li>Con y sin Piel</li>
-		<li>Aire Acondicionado</li>
-		<li>Reproductor de CD</li>
-		<li>Pantalla LCD</li>
-		<li>DVD</li>
-		<li>Seguro de viajero</li>
-	</ul>
+		<div class="row">
+			<div class="left-column">
+				<div class="great-luxury">
+					<img class="image-responsive" src="/images/unidades/suburban-de-gran-lujo-con-chofer.jpg" alt="Suburban de Gran Lujo con Chofer">
+				</div>
+			</div> <!-- /.left-column -->
+			<div class="right-column">
+				<h4 class="features-heading">Características Principales:</h4>
+				
+				<ul>
+					<li><i class="icon-circle"></i>Automática</li>
+					<li><i class="icon-circle"></i>Vidrios Eléctricos</li>
+					<li><i class="icon-circle"></i>Con y sin Piel</li>
+					<li><i class="icon-circle"></i>Aire Acondicionado</li>
+					<li><i class="icon-circle"></i>Reproductor de CD</li>
+					<li><i class="icon-circle"></i>Pantalla LCD</li>
+					<li><i class="icon-circle"></i>DVD</li>
+					<li><i class="icon-circle"></i>Seguro de viajero</li>
+				</ul>
+			</div> <!-- /.right-column -->
+		</div>
+	</div> <!-- /.content -->
 </div> <!-- /.units -->
