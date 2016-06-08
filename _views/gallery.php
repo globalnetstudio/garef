a29ac8d5ab0472b2ec9332a49f9091c5692cbd48
Daniel González Briseño <d.gonzalez@globalnetstudio.com>
2016-06-08 16:35:35 -0500

    Se terminó la sección de gallería

--- _views/gallery.php
+++ _views/gallery.php
@@ -1,3 +1,84 @@
 <div class="gallery">
-	<h1>Página de Galería</h1>
+	<div class="heading">
+		<div class="wrapper">
+			<h2>Galería</h2>
+		</div> <!-- /.wrapper -->
+	</div> <!-- /.heading -->
+
+	<div class="content">
+		<div class="heading-gallery">
+			<a class="left-chevron" href="#"></a>
+			<h3>Irizar I.6</h3>
+			<a class="right-chevron" href="#"></a>
+		</div> <!-- /.heading-gallery -->
+
+		<div class="irizar-gallery">
+			<div class="row">
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+			</div> <!-- /.row  -->
+		</div> <!-- /.irizar-gallery -->
+
+		<div class="heading-gallery">
+			<a class="left-chevron" href="#"></a>
+			<h3>Irizar P.b Autobus Negro</h3>
+			<a class="right-chevron" href="#"></a>
+		</div> <!-- /.heading-gallery -->
+
+		<div class="irizar-p-b-gallery">
+			<div class="row">
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+			</div> <!-- /.row  -->
+		</div> <!-- /.irizar-p-b-gallery -->
+
+		<div class="heading-gallery">
+			<a class="left-chevron" href="#"></a>
+			<h3>Irizar P.b Autobus Blanco</h3>
+			<a class="right-chevron" href="#"></a>
+		</div> <!-- /.heading-gallery -->
+
+		<div class="irizar-p-b-gallery">
+			<div class="row">
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+			</div> <!-- /.row  -->
+		</div> <!-- /.irizar-p-b-gallery -->
+
+		<div class="heading-gallery">
+			<a class="left-chevron" href="#"></a>
+			<h3>Sprinter</h3>
+			<a class="right-chevron" href="#"></a>
+		</div> <!-- /.heading-gallery -->
+
+		<div class="sprinter-gallery">
+			<div class="row">
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+			</div> <!-- /.row  -->
+		</div> <!-- /.sprinter-gallery -->
+
+		<div class="heading-gallery">
+			<a class="left-chevron" href="#"></a>
+			<h3>Suburban</h3>
+			<a class="right-chevron" href="#"></a>
+		</div> <!-- /.heading-gallery -->
+
+		<div class="suburban-gallery">
+			<div class="row">
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+				<div class="column"></div> <!-- /.column -->
+			</div> <!-- /.row  -->
+		</div> <!-- /.suburban-gallery -->
+	</div> <!-- /.content -->
 </div> <!-- /.gallery -->
